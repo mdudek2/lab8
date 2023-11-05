1a015ba238c7836885db166d4f27fd4f336efcd4
mdudek2 <mdudek2@hawk.iit.edu>
2023-11-05 01:26:46 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+  var alertbutton = document.getElementById('entrybutton');
+  
+  function displayText(){
+    var textbox = document.getElementById('entryinput');
+	document.getElementById('textoutput').innerHTML = textbox.value;
+	alert("Maks Dudek: " + textbox.value);
+  }
 
-
-
-
-
-
+  alertbutton.addEventListener('click', displayText);
+}
 
 window.addEventListener('load', init);
